2446f7640e6e715f8080cd388068188d9f4b5934
Matteo Moscone <matteo.moscone.1atgiorgi@gmail.com>
2025-05-20 18:24:04 +0200

    gfd

--- Class/Helper.php
+++ Class/Helper.php
@@ -16,7 +16,8 @@ class Helper
 
     public $menu_seasons = [
         'matches',
-        'ranking',
+        'table',
+        'scoreboard',
         'statistics',
     ];
 
@@ -107,7 +108,8 @@ class Helper
         return $row ? $row['nome'] : null;
     }
 
-    function getParamsbyID($id, $table, $cod = "id"){
+    function getParamsbyID($id, $table, $cod = "id")
+    {
         $r = $this->db->getOne("{$table}", "{$cod} = ?", [$id]);
         return $r['params'];
     }
@@ -135,43 +137,81 @@ class Helper
                         'gol_subiti' => 0,
                         'diff_reti' => 0,
                         'punti' => 0,
+                        'giocate_c' => 0,
+                        'vittorie_c' => 0,
+                        'pareggi_c' => 0,
+                        'sconfitte_c' => 0,
+                        'gol_fatti_c' => 0,
+                        'gol_subiti_c' => 0,
+                        'diff_reti_c' => 0,
+                        'punti_c' => 0,
+                        'giocate_t' => 0,
+                        'vittorie_t' => 0,
+                        'pareggi_t' => 0,
+                        'sconfitte_t' => 0,
+                        'gol_fatti_t' => 0,
+                        'gol_subiti_t' => 0,
+                        'diff_reti_t' => 0,
+                        'punti_t' => 0,
                     ];
                 }
             }
 
             // Aggiornamento statistiche
-            $classifica[$casa]['giocate']++;
-            $classifica[$trasferta]['giocate']++;
-            $classifica[$casa]['gol_fatti'] += $golCasa;
-            $classifica[$casa]['gol_subiti'] += $golTrasferta;
-            $classifica[$trasferta]['gol_fatti'] += $golTrasferta;
-            $classifica[$trasferta]['gol_subiti'] += $golCasa;
+            $classifica[$casa]['giocate_c']++;
+            $classifica[$trasferta]['giocate_t']++;
+            $classifica[$casa]['gol_fatti_c'] += $golCasa;
+            $classifica[$casa]['gol_subiti_c'] += $golTrasferta;
+            $classifica[$trasferta]['gol_fatti_t'] += $golTrasferta;
+            $classifica[$trasferta]['gol_subiti_t'] += $golCasa;
 
             // Calcolo della differenza reti separatamente
             $diffCasa = $golCasa - $golTrasferta;
             $diffTrasferta = $golTrasferta - $golCasa;
 
             // Aggiorno la differenza reti
-            $classifica[$casa]['diff_reti'] += $diffCasa;
-            $classifica[$trasferta]['diff_reti'] += $diffTrasferta;
+            $classifica[$casa]['diff_reti_c'] += $diffCasa;
+            $classifica[$trasferta]['diff_reti_t'] += $diffTrasferta;
 
             if ($golCasa > $golTrasferta) {
                 // Casa vince
-                $classifica[$casa]['vittorie']++;
-                $classifica[$trasferta]['sconfitte']++;
-                $classifica[$casa]['punti'] += 3;
+                $classifica[$casa]['vittorie_c']++;
+                $classifica[$trasferta]['sconfitte_t']++;
+                $classifica[$casa]['punti_c'] += 3;
             } elseif ($golCasa < $golTrasferta) {
                 // Trasferta vince
-                $classifica[$trasferta]['vittorie']++;
-                $classifica[$casa]['sconfitte']++;
-                $classifica[$trasferta]['punti'] += 3;
+                $classifica[$trasferta]['vittorie_t']++;
+                $classifica[$casa]['sconfitte_c']++;
+                $classifica[$trasferta]['punti_t'] += 3;
             } else {
                 // Pareggio
-                $classifica[$casa]['pareggi']++;
-                $classifica[$trasferta]['pareggi']++;
-                $classifica[$casa]['punti'] += 1;
-                $classifica[$trasferta]['punti'] += 1;
+                $classifica[$casa]['pareggi_c']++;
+                $classifica[$trasferta]['pareggi_t']++;
+                $classifica[$casa]['punti_c'] += 1;
+                $classifica[$trasferta]['punti_t'] += 1;
             }
+
+            // Calcolo totale per ciascuna squadra (casa + trasferta)
+            $classifica[$casa]['giocate'] = $classifica[$casa]['giocate_c'] + $classifica[$casa]['giocate_t'];
+            $classifica[$casa]['punti'] = $classifica[$casa]['punti_c'] + $classifica[$casa]['punti_t'];
+            $classifica[$casa]['vittorie'] = $classifica[$casa]['vittorie_c'] + $classifica[$casa]['vittorie_t'];
+            $classifica[$casa]['pareggi'] = $classifica[$casa]['pareggi_c'] + $classifica[$casa]['pareggi_t'];
+            $classifica[$casa]['sconfitte'] = $classifica[$casa]['sconfitte_c'] + $classifica[$casa]['sconfitte_t'];
+            $classifica[$casa]['gol_fatti'] = $classifica[$casa]['gol_fatti_c'] + $classifica[$casa]['gol_fatti_t'];
+            $classifica[$casa]['gol_subiti'] = $classifica[$casa]['gol_subiti_c'] + $classifica[$casa]['gol_subiti_t'];
+            $classifica[$casa]['diff_reti'] = $classifica[$casa]['diff_reti_c'] + $classifica[$casa]['diff_reti_t'];
+
+            // Totale della squadra in trasferta
+            $classifica[$trasferta]['giocate'] = $classifica[$trasferta]['giocate_c'] + $classifica[$trasferta]['giocate_t'];
+            $classifica[$trasferta]['punti'] = $classifica[$trasferta]['punti_c'] + $classifica[$trasferta]['punti_t'];
+            $classifica[$trasferta]['vittorie'] = $classifica[$trasferta]['vittorie_c'] + $classifica[$trasferta]['vittorie_t'];
+            $classifica[$trasferta]['pareggi'] = $classifica[$trasferta]['pareggi_c'] + $classifica[$trasferta]['pareggi_t'];
+            $classifica[$trasferta]['sconfitte'] = $classifica[$trasferta]['sconfitte_c'] + $classifica[$trasferta]['sconfitte_t'];
+            $classifica[$trasferta]['gol_fatti'] = $classifica[$trasferta]['gol_fatti_c'] + $classifica[$trasferta]['gol_fatti_t'];
+            $classifica[$trasferta]['gol_subiti'] = $classifica[$trasferta]['gol_subiti_c'] + $classifica[$trasferta]['gol_subiti_t'];
+            $classifica[$trasferta]['diff_reti'] = $classifica[$trasferta]['diff_reti_c'] + $classifica[$trasferta]['diff_reti_t'];
+
+
         }
 
         // Ordinamento classifica
@@ -185,5 +225,81 @@ class Helper
 
         return $classifica;
     }
+
+    /**
+     * Per ogni metrica (vittorie, pareggi, …) e ambito ('', '_c', '_t'),
+     * calcola valore min e max e quali squadre li hanno ottenuti.
+     *
+     * @param array $classifica Array di squadre da getClassifica()
+     * @return array [
+     *   'min' => [
+     *     'vittorie'   => ['value'=>int, 'teams'=>[...]],
+     *     'vittorie_c' => ['value'=>int, 'teams'=>[...]],
+     *     …,
+     *   ],
+     *   'max' => [
+     *     'vittorie'   => ['value'=>int, 'teams'=>[...]],
+     *     'vittorie_c' => ['value'=>int, 'teams'=>[...]],
+     *     …,
+     *   ],
+     * ]
+     */
+    function getStatistics(array $classifica): array
+    {
+        $metrics = [
+            'vittorie',
+            'pareggi',
+            'sconfitte',
+            'gol_fatti',
+            'gol_subiti',
+            'diff_reti'
+        ];
+        $scopes = [
+            '' => '',    // totale
+            '_c' => '_c',  // casa
+            '_t' => '_t',  // trasferta
+        ];
+
+        $result = ['min' => [], 'max' => []];
+
+        // Inizializza con valori estremi e squadre vuote
+        foreach ($scopes as $suffix) {
+            foreach ($metrics as $metric) {
+                $key = $metric . $suffix;
+                $result['min'][$key] = ['value' => PHP_INT_MAX, 'teams' => []];
+                $result['max'][$key] = ['value' => PHP_INT_MIN, 'teams' => []];
+            }
+        }
+
+        // Scorri tutte le squadre
+        foreach ($classifica as $teamData) {
+            $teamId = $teamData['squadra_id'];
+            foreach ($scopes as $suffix) {
+                foreach ($metrics as $metric) {
+                    $key = $metric . $suffix;
+                    $value = $teamData[$key];
+
+                    // Minimo
+                    if ($value < $result['min'][$key]['value']) {
+                        $result['min'][$key]['value'] = $value;
+                        $result['min'][$key]['teams'] = [$teamId];
+                    } elseif ($value === $result['min'][$key]['value']) {
+                        $result['min'][$key]['teams'][] = $teamId;
+                    }
+
+                    // Massimo
+                    if ($value > $result['max'][$key]['value']) {
+                        $result['max'][$key]['value'] = $value;
+                        $result['max'][$key]['teams'] = [$teamId];
+                    } elseif ($value === $result['max'][$key]['value']) {
+                        $result['max'][$key]['teams'][] = $teamId;
+                    }
+                }
+            }
+        }
+
+        return $result;
+    }
+
 
 }
--- Menu/seasons_details.php
+++ Menu/seasons_details.php
@@ -16,11 +16,11 @@ $activeTab = $_GET['tab'] ?? 'matches';
 function generate($tab, $help, $langfile, $db)
 {
     $partite = $db->getAll("partite", '*', 'stagione_id = ?', [$_GET['season_id']], 'giornata ASC, data_partita ASC');
+    $classifica = $help->getClassifica($partite);
     switch ($tab) {
-        case 'ranking':
-            $classifica = $help->getClassifica($partite);
+        case 'table':
             ?>
-            <div class="table-responsive">
+            <div class="table-responsive mytable">
                 <table class="table table-hover table-striped align-middle text-center">
                     <thead class="table-dark sticky-top">
                         <tr>
@@ -75,22 +75,120 @@ function generate($tab, $help, $langfile, $db)
             <?php
             break;
 
-        case 'statistics':
+        case 'scoreboard':
+            ?>
+            <div class="table-responsive" style="max-width:100vw; margin-inline: calc((95vw - 100%) / -2);">
+                <table class="table table-hover table-striped align-middle text-center">
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <?php
+                            // Ottieni le squadre dalla stagione
+                            $squadre = json_decode($db->getOne("stagioni", "codice_stagione = ?", [$_GET['season_id']])['squadre']);
 
+                            // Crea un array associativo (ID => Nome)
+                            $squadrename = [];
+                            foreach ($squadre as $squadra) {
+                                $squadrename[$squadra] = $help->getTeamNameByID($squadra);
+                            }
+
+                            // Ordina le squadre per nome
+                            asort($squadrename); // Usa asort per ordinare per valore (nome squadra) mantenendo la relazione con gli ID
+                
+                            // Stampa i nomi delle squadre come intestazione
+                            foreach ($squadrename as $key => $squadra) {
+                                $params = json_decode($help->getParamsbyID($key, "squadre"));
+
+                                echo "<td><div class='rounded-pill fw-bold px-4 py-2' style='background-color: " . $params->colore_sfondo . "; color: " . $params->colore_testo . "; border: 1px solid " . $params->colore_bordo . ";'>" . substr($squadra, 0, 3) . "</div></td>";
+                                // Mostra i primi 3 caratteri del nome della squadra
+                            }
+                            ?>
+                        </tr>
+                        <?php
+                        // Ora stampiamo i risultati per ogni squadra
+                        foreach ($squadrename as $keyC => $squadra):
+                            $params = json_decode($help->getParamsbyID($keyC, "squadre"));
+                            echo "<tr>";
+                            echo "<td><div class='rounded-pill fw-bold px-4 py-2' style='background-color: " . $params->colore_sfondo . "; color: " . $params->colore_testo . "; border: 1px solid " . $params->colore_bordo . ";'>" . $squadra . "</div></td>"; // Mostra il nome della squadra nella prima colonna
+            
+                            // Per ogni avversaria nella colonna, cerchiamo il risultato della partita
+                            foreach ($squadrename as $keyT => $avversaria):
+                                $risultato = "-"; // Default se non esiste partita tra le due squadre
+            
+                                // Ottieni gli ID reali delle squadre (già associati)
+                                $squadraCasaID = $keyC; // ID della squadra di casa
+                                $squadraTrasfertaID = $keyT; // ID della squadra trasferta
+            
+                                // Cerca se c'è una partita tra le due squadre
+                                foreach ($partite as $partita) {
+                                    // Controlla se la partita coinvolge entrambe le squadre
+                                    if ($partita['squadra_casa_id'] == $squadraCasaID && $partita['squadra_trasferta_id'] == $squadraTrasfertaID) {
+                                        // Trova il risultato della partita
+                                        if ($partita['squadra_casa_id'] == $squadraCasaID) {
+                                            $risultato = $partita['gol_casa'] . "-" . $partita['gol_trasferta']; // Squadra di casa vince/perde
+                                        } else {
+                                            $risultato = $partita['gol_trasferta'] . "-" . $partita['gol_casa']; // Squadra ospite vince/perde
+                                        }
+                                        break; // Esci dal ciclo quando trovi la partita
+                                    }
+                                }
+
+                                // Mostra il risultato o il trattino se non esiste partita
+                                echo "<td>" . $risultato . "</td>";
+                            endforeach;
+                            echo "</tr>";
+                        endforeach;
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+
+            <?php
             break;
+
+        case 'statistics':
+            $statistiche = $help->getStatistics($classifica);
+            ?>
+            <div class="table-responsive">
+                <table class="table table-hover table-striped align-middle text-center">
+                    <thead>
+                        <tr>
+                            <th scope="col">Statistica</th>
+                            <th scope="col">Minimo (Valore & Squadre)</th>
+                            <th scope="col">Massimo (Valore & Squadre)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($statistiche['min'] as $key => $minData):
+                            $maxData = $statistiche['max'][$key];
+                            $minTeams = implode(', ', $minData['teams']);
+                            $maxTeams = implode(', ', $maxData['teams']);
+                            ?>
+                            <tr>
+                                <th scope="row"><?= htmlspecialchars($help->getTranslation($key, $langfile)) ?></th>
+                                <td>
+                                    <strong><?= htmlspecialchars($minData['value']) ?></strong><br>
+                                    <small><?= htmlspecialchars($minTeams) ?></small>
+                                </td>
+                                <td>
+                                    <strong><?= htmlspecialchars($maxData['value']) ?></strong><br>
+                                    <small><?= htmlspecialchars($maxTeams) ?></small>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <?php
+            break;
+
+
 
         default:
             // raggruppa le partite per giornata
             $grouped = [];
             foreach ($partite as $m) {
                 $grouped[$m['giornata']][] = $m;
-            }
-
-            // helper per ottenere il nome di una squadra
-            function getTeamName($id, $db)
-            {
-                $r = $db->getOne('squadre', 'id = ?', [$id]);
-                return $r ? $r['nome'] : '—';
             }
             ?>
             <div class="container">
@@ -104,36 +202,55 @@ function generate($tab, $help, $langfile, $db)
                         <div class="row">
                         <?php endif; ?>
 
-                        <div class="col-md-6 mb-4">
-                            <div class="card h-100">
-                                <!-- header -->
-                                <div class="card-header bg-primary text-white text-center fw-bold h3">
-                                    <?= $help->getTranslation('day', $langfile) . " " . htmlspecialchars($giornata) ?>
-                                </div>
-                                <!-- body -->
-                                <div class="card-body">
+                        <div class="col mb-4">
+                            <h2 class="text-center fw-bold"><?= $help->getTranslation('day', $langfile) . " " . $giornata ?></h2>
+                            <table class="table table-striped table-hover matchtable">
+                                <thead>
+                                    <tr class="text-center">
+                                        <th scope="col">Data</th>
+                                        <th scope="col">Squadra Casa</th>
+                                        <th scope="col">Squadra Trasferta</th>
+                                        <th scope="col" class="text-center">Risultato</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
                                     <?php foreach ($matches as $m):
+                                        // Formattazione data
                                         $date = $m['data_partita']
                                             ? date('d/m/Y', strtotime($m['data_partita']))
                                             : '';
-                                        $home = htmlspecialchars(getTeamName($m['squadra_casa_id'], $db));
-                                        $away = htmlspecialchars(getTeamName($m['squadra_trasferta_id'], $db));
+
+                                        // Parametri colori squadre
+                                        $params1 = json_decode($help->getParamsbyID($m['squadra_casa_id'], "squadre"));
+                                        $params2 = json_decode($help->getParamsbyID($m['squadra_trasferta_id'], "squadre"));
+
+                                        // Badge “pill” squadre
+                                        $home = "<span class='rounded-pill fw-bold px-3 py-1 text-nowrap'
+                     style='background-color: {$params1->colore_sfondo};
+                            color: {$params1->colore_testo};
+                            border: 1px solid {$params1->colore_bordo};'>
+                   " . htmlspecialchars($help->getTeamNameByID($m['squadra_casa_id'])) . "
+               </span>";
+                                        $away = "<span class='rounded-pill fw-bold px-3 py-1 text-nowrap'
+                     style='background-color: {$params2->colore_sfondo};
+                            color: {$params2->colore_testo};
+                            border: 1px solid {$params2->colore_bordo};'>
+                   " . htmlspecialchars($help->getTeamNameByID($m['squadra_trasferta_id'])) . "
+               </span>";
+
+                                        // Punteggio
                                         $score = intval($m['gol_casa']) . '‑' . intval($m['gol_trasferta']);
-                                        echo "
-                                            <div class=\"d-flex flex-wrap justify-content-between align-items-center py-2 border-bottom\">
-                                                <div class=\"d-flex flex-column flex-md-row align-items-start align-items-md-center gap-2\">
-                                                    <span class=\"text-muted small\">{$date}</span>
-                                                    <span class=\"fw-semibold\">{$home} – {$away}</span>
-                                                </div>
-                                                <div class=\"fw-bold fs-5 text-end text-md-start\" style=\"min-width: 70px;\">
-                                                    {$score}
-                                                </div>
-                                            </div>";
-                                    endforeach; ?>
-                                </div>
-                                <!-- footer (vuoto per ora) -->
-                                <div class="card-footer"></div>
-                            </div>
+                                        ?>
+                                        <tr>
+                                            <td class="align-middle"><?= $date ?></td>
+                                            <td class="align-middle text-center"><?= $home ?></td>
+                                            <td class="align-middle text-center"><?= $away ?></td>
+                                            <td class="align-middle text-center fw-bold"><?= $score ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+
                         </div>
                         <?php
                         $idx++;
@@ -151,7 +268,7 @@ function generate($tab, $help, $langfile, $db)
 
     <div class="row mb-4">
         <?php foreach ($help->menu_seasons as $m): ?>
-            <div class="col-12 col-md-6 col-lg-4">
+            <div class="col">
                 <div class="card mb-3 shadow-sm">
                     <div class="card-body text-center">
                         <a href="?page=seasons_details&season_id=<?= urlencode($_GET['season_id']) ?>&tab=<?= $m ?>"
